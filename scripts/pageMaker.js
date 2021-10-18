982494e0f9e2fb0d6fa169387b1d60d49b6e07c0
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2021-10-18 14:46:06 +0100

    Created a script to add links and the nav bar to the top of every page

---
+++ scripts/pageMaker.js
@@ -0,0 +1,15 @@
+
+$("#topOfPage").append(`
+		<a href="createAccount.html"> Create Account</a>
+		<a href="login.html"> Login</a>
+
+		<h1> Welcome to the `+$("title").text()+`</h1>
+
+		<nav>
+			<a href="index.html"> Home</a>
+			<a href="timetable.html"> Timetable</a>
+			<a href="classes.html"> Classes</a>
+			<a href="news.html"> News</a>
+			<a href="about.html"> About</a>
+		</nav>
+`);

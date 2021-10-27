c07f02b8228b6fb8ff8648065495acf0470d1566
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2021-10-27 20:29:15 +0100

    Made some slight style changes
    Changed to new backgroundles logos, centered some text and moved some links

--- scripts/pageMaker.js
+++ scripts/pageMaker.js
@@ -1,8 +1,10 @@
 
 $("#topOfPage").append(`
 		<a id="skipToContent" href="#main"> Skip to content </a>
-		<a href="createAccount.html"> Create Account</a>
-		<a href="login.html"> Login</a>
+		<div id="loginLinks">
+			<a href="createAccount.html"> Create Account</a>
+			<a href="login.html"> Login</a>
+		</div>
 
 		<h1> Welcome to the `+$("title").text()+`</h1>
 
@@ -20,9 +22,9 @@ $("#bottomOfPage").append(`
 	</br>
 	<h2> Contact Us </h2>
 
-	<a href="https://www.facebook.com"> <img src="../resources/fblogo.png" alt="Facebook logo" height="10%" width="10%"> </a>
-	<a href="https://www.twitter.com"> <img src="../resources/twlogo.png" alt="Twitter logo" height="10%" width="10%"> </a>
-	<a href="https://www.instagram.com"> <img src="../resources/inlogo.jpg" alt="Instagram logo" height="10%" width="10%"> </a>
-	<a href="mailto:c19753889@mytudublin.ie"> <img src="../resources/ml.png" alt="Mail logo" height="10%" width="10%"> </a>
+	<a href="https://www.facebook.com"> <img src="../resources/fblogo.png" alt="Facebook logo" height="3%" width="3%"> </a>
+	<a href="https://www.twitter.com"> <img src="../resources/twlogo.png" alt="Twitter logo" height="3%" width="3%"> </a>
+	<a href="https://www.instagram.com"> <img src="../resources/inlogo.png" alt="Instagram logo" height="3%" width="3%"> </a>
+	<a href="mailto:c19753889@mytudublin.ie"> <img src="../resources/ml.png" alt="Mail logo" height="3%" width="3%"> </a>
 
 `);

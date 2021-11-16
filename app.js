150df36dd43f15b0ba09713738ec7253d3882d89
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2021-11-16 13:42:29 +0000

    Added create form and quick links for going home and switching forms

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pgp = require('pg-promise');
-const {PreparedStatement: PS} = require('pg-promise');
-const db = pgp('postgres://postgres:password@host:5432/');
+const { body, validationResult } = require('express-validator');
+
+const pgp = require('pg-promise')({});
+const { PreparedStatement: PS } = require('pg-promise');
+// connection = protocol://userName:password@host:port/databaseName
+const db = pgp('postgres://postgres:password@localhost:5432/lab6');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
 app.listen(3000, function() {
     console.log('Server running on port 3000');
 });
 
 //Sets routs for nav bar
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/pages/index.html");
+    res.sendFile(__dirname + "/public/pages/index.html");
 });
 app.get("/about", function(req, res) {
-    res.sendFile(__dirname + "/pages/about.html");
+    res.sendFile(__dirname + "/public/pages/about.html");
 });
 app.get("/classes", function(req, res) {
-    res.sendFile(__dirname + "/pages/classes.html");
+    res.sendFile(__dirname + "/public/pages/classes.html");
 });
 app.get("/createAccount", function(req, res) {
-    res.sendFile(__dirname + "/pages/createAccount.html");
+    res.sendFile(__dirname + "/public/pages/createAccount.html");
 });
 app.get("/login", function(req, res) {
-    res.sendFile(__dirname + "/pages/login.html");
+    res.sendFile(__dirname + "/public/pages/login.html");
 });
 app.get("/news", function(req, res) {
-    res.sendFile(__dirname + "/pages/news.html");
+    res.sendFile(__dirname + "/public/pages/news.html");
 });
 app.get("/timeTable", function(req, res) {
-    res.sendFile(__dirname + "/pages/timeTable.html");
+    res.sendFile(__dirname + "/public/pages/timeTable.html");
 });
 

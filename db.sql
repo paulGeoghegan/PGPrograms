d5c35e615028080fe1f79b343afcc22063cb5a77
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2021-11-16 14:28:28 +0000

    Creating sql script

--- db.sql
+++ db.sql
@@ -1,3 +1,13 @@
-CREATE DATABASE pgPrograms
+--run the below command to create your database then connect to it and you can run the rest of the file
+--CREATE DATABASE pgPrograms
 
-CREATE TABLE users(id serial primary key, name varchar(50), email varchar(50) UNIQUE, password varchar(50));
+--Drops tables
+DROP TABLE users;
+DROP TABLE classes;
+
+--Creates users table
+CREATE TABLE users(userId serial primary key, name varchar(50), email varchar(50) UNIQUE, password varchar);
+
+--Creates classes table and inserts some rows
+CREATE TABLE classes(classId serial primary key, className varchar, classDescription varchar);
+INSERT INTO classes('Programming Basics', 'This is an introductory programming course which will take you through the basics of programming');

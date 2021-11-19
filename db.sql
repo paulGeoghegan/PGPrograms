84288056ab44133519ccad422893d699cb16e743
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2021-11-19 16:36:52 +0000

    Added a table on the classes page which will show the list of available classes

--- db.sql
+++ db.sql
@@ -9,7 +9,7 @@ DROP TABLE classes;
 CREATE TABLE users(userId serial primary key, name varchar(50), email varchar(50) UNIQUE, password varchar);
 
 --Creates classes table and inserts some rows
-CREATE TABLE classes(classId serial primary key, className varchar, classDescription varchar);
-INSERT INTO classes(classname, classdescription) values('Programming Basics', 'This is an introductory programming course which will take you through the basics of programming');
-INSERT INTO classes(classname, classdescription) values('Intermediat Programming', 'This is a followon to the beginner programming class. This class dives more indept into the programming fundimentals and starts to explore concepts such as Object Orientated Programming.');
-
+CREATE TABLE classes(classId serial primary key, className varchar, classDescription varchar, classlevel varchar);
+INSERT INTO classes(classname, classdescription, classlevel) values('Intro to Programming', 'This is an introductory programming course which will take you through the basics of programming', 'Basic');
+INSERT INTO classes(classname, classdescription, classlevel) values('Object Orientated Programming', 'This is a follow on to the beginner programming class. This class dives more indept into the programming fundimentals and starts to explore concepts such as Object Orientated Programming.', 'Intermediate');
+INSERT INTO classes(classname, classdescription, classlevel) values('Introduction to AI Programming', 'This programming class will explore advanced concepts such as machine learning and AI', 'advanced');
---
+++ public/scripts/classes.js
@@ -0,0 +1,50 @@
+
+if(document.readyState)
+{
+    printClassList();
+}
+
+/*$(document).on("click", "a") function(a)
+{
+    console.log(this.a);
+}
+*/
+
+//This function sends the request to get the database rows for the classes page
+function printClassList()
+{
+    const get = $.get('http://localhost:3000/classeslist');
+    get.done(processResults);
+    get.fail(processErrors);
+}
+
+//Logs the errors
+function processErrors() {
+    console.log('Validation errors');
+}
+
+//Prints rows to the page
+function processResults(rows, status, xhr) {
+    console.log('Data sent to the server');
+    let classtable = `
+    <table id="classTable">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Level</th>
+        </tr>
+    </thead>
+    <tbody>`;
+
+    //Loops through rows and prints them
+    for (let i = 0; i < rows.length; i++) {
+        classtable += `<tr><td> <a id="${rows[i].classid}" href="localhost:3000/class"> ${rows[i].classname} </a></td>`
+        classtable += `<td>${rows[i].classlevel}</td></tr>`
+    }
+    classtable += `
+        </tbody>
+    </table>`;
+
+    //Appends table
+    $(classtable).appendTo('#middleOfPage');
+}
